5efbebd31b41c5c8b1b91ff34e580f0af1544123
Kreckeroff <Kreckeroff@users.noreply.github.com>
2020-06-11 00:51:59 +0300

    commit task2

--- homeworks/lesson1/task2.py
+++ homeworks/lesson1/task2.py
@@ -1,1 +1,13 @@
-#test
+#program to convert time to format hh:mm:ss
+print("this program is designed to convert time into a format hh:mm:ss")
+while True:
+    time = input("write the time in seconds: ")
+    if time.isdigit():
+        time = int(time)
+        break
+    print("error, write a integer number")
+hours = time // 3600
+minute = (time - hours * 3600) // 60
+seconds = (time - hours * 3600 - minute * 60)
+result = ('time format hh:mm:ss {hours} : {minute} : {seconds}').format(hours=hours, minute=minute, seconds=seconds)
+print(result)
