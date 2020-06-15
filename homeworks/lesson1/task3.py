45a03582dce34e5c68472d09002d0d97173943bc
Egor <31611609+Kreckeroff@users.noreply.github.com>
2020-06-15 19:21:47 +0300

    Delete task3.py

--- homeworks/lesson1/task3.py
+++
@@ -1,9 +0,0 @@
-#this program is looking for the sum n+nn+nnn of the number n
-while True:
-    n = input("write the number: ")
-    if n.isdigit():
-        n = int(n)
-        break
-    print("error, write a integer number")
-result = (n + int(str(n)+str(n)) + int(str(n) + str(n) +str(n)))
-print("n + nn + nnn = ",result)
