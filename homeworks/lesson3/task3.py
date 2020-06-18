0949be13a678f268194964b3bd769ca1d61d291a
Kreckeroff <Kreckeroff@users.noreply.github.com>
2020-06-18 20:52:00 +0300

    upload task1.py

--- homeworks/lesson3/task3.py
+++ homeworks/lesson3/task3.py
@@ -5,7 +5,7 @@ def my_sum(arg_1, arg_2, arg_3):
         return arg_2 + arg_3
     else:
         return arg_3 + arg_1
-print(f'{my_sum(int(input("write first arg")), int(input("write 2 arg")), int(input("write 3 arg")))}')
+print(f'{my_sum(int(input("write first arg: ")), int(input("write 2 arg: ")), int(input("write 3 arg: ")))}')
 
 
 
