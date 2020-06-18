364e1216d92658a0c8ae8f647054f50510e2c98e
Kreckeroff <Kreckeroff@users.noreply.github.com>
2020-06-18 20:52:53 +0300

    upload task1.py

--- homeworks/lesson3/task1.py
+++ homeworks/lesson3/task1.py
@@ -1,5 +1,5 @@
 #1'st task
-def my_sum(*args):
+def my_div(*args):
     print('this function will count division 1 argument by 2')
     arg_1 = float(input('write 1 arg: '))
     arg_2 = float(input('write 2 arg: '))
@@ -7,4 +7,4 @@ def my_sum(*args):
         print('Enter non zero arg: ')
         arg_2 = float(input('write arg: '))
     return(arg_1 / arg_2)
-print(f'{my_sum()}')
+print(f'{my_div()}')
--- homeworks/lesson3/task3.py
+++ homeworks/lesson3/task3.py
@@ -6,6 +6,3 @@ def my_sum(arg_1, arg_2, arg_3):
     else:
         return arg_3 + arg_1
 print(f'{my_sum(int(input("write first arg: ")), int(input("write 2 arg: ")), int(input("write 3 arg: ")))}')
-
-
-
